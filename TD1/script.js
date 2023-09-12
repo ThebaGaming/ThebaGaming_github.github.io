479e427ab53b39afe12abe982adf383b3b6ec07b
dt211441 <theodurand5@gmail.com>
2023-09-12 10:46:41 +0200

    Exos 2

--- TD1/script.js
+++ TD1/script.js
@@ -1,13 +1,25 @@
 let button = document.getElementById("get-location");
 let latText = document.getElementById("latitude");
 let longText = document.getElementById("longitude");
+let altText = document.getElementById("altitude");
+let speText = document.getElementById("speed");
 
 button.addEventListener("click", () => {
   navigator.geolocation.getCurrentPosition((position) => {
     let lat = position.coords.latitude;
     let long = position.coords.longitude;
+    let alt = position.coords.altitude;
+    let speed = position.coords.speed;
 
     latText.innerText = lat.toFixed(2);
     longText.innerText = long.toFixed(2);
+    altText.innerText = alt.toFixed(2);
+    speText.innerText = speed.toFixed(2)
   });
 });
+
+let watch_button = document.getElementById("watch_button");
+
+watch_button.addEventListener("click", ()=> {
+    navigator.geolocation.watchPosition()
+})
